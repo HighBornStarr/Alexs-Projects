98956e6ed501cbec5c557b784dfbe7d325e11c33
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2022-11-25 13:44:25 -0600

    Finished

--- app.js
+++ app.js
@@ -5,20 +5,32 @@ const inputEl= document.getElementById("input") // input element attached
 const questionEl = document.getElementById("question"); // declare variable and attach document to question ID
 questionEl.innerText = `What is ${num1} multiplied by ${num2}? `  // print questionEL to innertext with question and num variables
 
-let correctAns = num1 * num2  // answer variable 
+let correctAns = num1 * num2;  // answer variable 
+const scoreEl = document.getElementById('score'); // attach scoreEL
 
-let score = 0  //  declare score at 0 at start of game. 
+
+let score = JSON.parse(localStorage.getItem("score"));  //  declare score at localStorage var at start of game. Use JSON.parse to turn into number from string .
+
+if(!score){
+  score = 0
+}
+
+scoreEl.innerText = `Score: ${score}` // print out score var being added or minused on app.
 
 formEl.addEventListener("submit", () => {  // add event listner upon click of submit button
 const userAns = +inputEl.value
 if(userAns === correctAns){  // if user's Answer is same as correct answer variable increment score if not decrement score
 score++;
-console.log(score)
+UpdateStorage()
 } else {
   score--;
-  console.log(score)
+  UpdateStorage()
 }
 ;
 });
+
+function UpdateStorage() {
+  localStorage.setItem("score",JSON.stringify(score)) // store variables score and turn it into string as browser will only store strings for security purposes.
+}
 
 
